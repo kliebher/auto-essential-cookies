b3113a3ea3453addb269d619bdca2ff2f88748ba
Kjell Liebher <kjell-liebher@gmx.de>
2024-06-06 13:53:03 +0200

    added check to avoid unnecessary origin script execution

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,27 +1,25 @@
-import { SESSION_STORAGE, LOADING_TIMEOUT } from "./config";
+import { SESSION_STORAGE, INITIAL_TIMEOUT } from "./config";
 import { ProcessManager } from "./process";
-
-let MAX_RETRY = 3
 
 async function run() {
     setTimeout(async () => {
         await new ProcessManager().init()
-        const status = SESSION_STORAGE.get('AEC')
-        if (status === 'done') return
-        if (MAX_RETRY-- > 0) {
-            await run()
-        } else {
-            SESSION_STORAGE.set('AEC', 'done')
-            return
-        }
-    }, LOADING_TIMEOUT)
+    }, INITIAL_TIMEOUT)
 }
 
 async function main() {
-    if (SESSION_STORAGE.get('AEC') === null){
-        SESSION_STORAGE.set('AEC', 'in_progress')
+    if (SESSION_STORAGE.get('AEC') === null && isVisible(document.documentElement)){
+        // SESSION_STORAGE.set('AEC', 'in_progress')
         await run()
     }
+}
+
+function isVisible (el: HTMLElement | Element) {
+    const style = window.getComputedStyle(el)
+    // if (style.width === '0px' || style.height === '0px') return false;
+    if (style.width === 'auto' && style.height === 'auto') return false
+    else if (style.display === 'none') return false
+    return true
 }
 
 window.onload = main
